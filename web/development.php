302712fe9b1d98d3f8aa88837daa910ec8ad5656
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2002-06-10 14:23:53 +0000

    fixed bug report and feature request links

--- web/development.php
+++ web/development.php
@@ -8,8 +8,8 @@ Audiere's development tools are hosted on
 <a href="http://sf.net/">SourceForge</a>.  The
 <a href="http://sf.net/projects/audiere/">project page</a> has
 <a href="http://sourceforge.net/project/showfiles.php?group_id=32783">downloads</a>,
-<a href="http://sourceforge.net/tracker/?atid=439230&amp;group_id=32783&amp;func=browse">bug reports</a>,
-<a href="http://sourceforge.net/tracker/?atid=439233&amp;group_id=32783&amp;func=browse">feature requests</a>,
+<a href="http://sourceforge.net/tracker/?atid=406494&amp;group_id=32783&amp;func=browse">bug reports</a>,
+<a href="http://sourceforge.net/tracker/?atid=406497&amp;group_id=32783&amp;func=browse">feature requests</a>,
 <a href="http://sourceforge.net/mail/?group_id=32783">mailing lists</a>, a
 <a href="http://sourceforge.net/pm/?group_id=32783">task manager</a>, and a web-based
 <a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/audiere">CVS viewer</a>.
