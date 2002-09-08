194650ccdf6c63859955db624048fcabbfdc3649
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2002-09-08 17:29:19 +0000

    make the download page a little clearer

--- web/download.php
+++ web/download.php
@@ -3,15 +3,17 @@ $current_page = 'download';
 include 'page_header.inc';
 ?>
 
-<ul>
+<h3>1.9.0</h3>
 
+<ul>
 <?php
 download('audiere-1.9.0-win32.zip',         'Win32 Binary Release');
 download('audiere-1.9.0.tar.gz',      'UNIX Source Release');
 download('audiere-1.9.0-src.tar.bz2', 'Source Snapshot');
 ?>
-
 </ul>
+
+<h3>Older Files</h3>
 
 <p>
 All files are available at the <a
