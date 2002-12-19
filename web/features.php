d2c743c09c50e59cdf6bba0c76b3881402d73b31
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2002-12-19 09:41:54 +0000

    adding page to list features of Audiere

---
+++ web/features.php
@@ -0,0 +1,23 @@
+<?php
+$current_page = 'features';
+include 'page_header.inc';
+?>
+
+<ul>
+<li>Braindead easy API</li>
+<li>Supported file formats:
+        Uncompressed WAV*,
+        <a href="http://vorbis.com/">Ogg Vorbis</a>*,
+        <a href="http://flac.sourceforge.net/">FLAC</a>*,
+        MP3 (* supports seeking)</li>
+<li>Streaming and buffered audio</li>
+<li>Volume, pan, and pitch shift modification</li>
+<li>Flat tone, square wave, white noise, and pink noise generation</li>
+<li>Runtime enumeration of supported file formats</li>
+<li>Custom file streams</li>
+<li>Python, Java, and XPCOM (JavaScript in Mozilla) bindings<li>
+</ul>
+
+<?php
+include 'page_footer.inc';
+?>
