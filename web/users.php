2bfd89d053ce03a28271149ce469e3d1a7c72df9
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2004-02-03 22:04:27 +0000

    adding Supa-G to users page

--- web/users.php
+++ web/users.php
@@ -16,6 +16,7 @@ Projects that use Audiere are listed here.
 <li><a href="http://isugamedev.sf.net/">ISU Game Developers Club</a></li>
 <li><a href="http://www.jarroddavis.com/">Jarrod Davis Software</a></li>
 <li><a href="http://aegisknight.org/sphere">Sphere</a></li>
+<li><a href="http://www.supa-g.com/">Supa-G</a></li>
 <li><a href="http://pubpages.unh.edu/~shirokov/ssr/">Super Slot Racers</a></li>
 <li><a href="http://ikagames.com/andy">v26 and neoverge</a></li>
 <li><a href="http://valengames.com/">Valen Games</a></li>
