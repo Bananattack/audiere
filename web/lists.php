5cd7e80e92c38c5f29037af52263d4ca65c71e68
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2004-03-19 06:27:58 +0000

    fixing mailing list archive links

--- web/lists.php
+++ web/lists.php
@@ -2,10 +2,10 @@
 $current_page = 'lists';
 include 'page_header.inc';
 
-function mailing_list($name, $description) {
+function mailing_list($name, $id, $description) {
   $mailto    = 'mailto:' . $name . '@lists.sf.net';
   $subscribe = 'http://lists.sourceforge.net/lists/listinfo/' . $name;
-  $archive   = 'http://www.geocrawler.com/redir-sf.php3?list=' . $name;
+  $archive   = 'http://sourceforge.net/mailarchive/forum.php?forum_id=' . $id;
     
   echo '<tr>';
   echo "<td><a href=\"$mailto\">$name</a></td>";
@@ -18,15 +18,15 @@ function mailing_list($name, $description) {
 }
 
 echo '<table>';
-mailing_list('audiere-annc', 'Announcements and news related to Audiere.  ' .
+mailing_list('audiere-annc', '6489', 'Announcements and news related to Audiere.  ' .
              'This is a low-volume list for those who are only interested in ' .
              'new releases.');
-mailing_list('audiere-users', 'User information and discussion.  Use this list ' .
+mailing_list('audiere-users', '6496', 'User information and discussion.  Use this list ' .
              'to get in contact with other Audiere users.');
-mailing_list('audiere-devel', 'Developer discussion.  This list is for discussion ' .
+mailing_list('audiere-devel', '292', 'Developer discussion.  This list is for discussion ' .
              'about the development process and other topics the average user ' .
              'does not care about.  Use this list to reach the developers.');
-mailing_list('audiere-commits', 'Relatively high-volume list that logs all CVS ' .
+mailing_list('audiere-commits', '33346', 'Relatively high-volume list that logs all CVS ' .
              'commits.  Subscribe to this list if you want to see every change ' .
              'to the source code when they happen.');
 echo '</table>';
